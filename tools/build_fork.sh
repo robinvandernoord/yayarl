6476ec4f8d4874870b6207d54f33141b2d9bce1a
Robin van der Noord <robinvandernoord@gmail.com>
2025-09-09 15:18:01 +0200

    chore: update build process

--- tools/build_fork.sh
+++ tools/build_fork.sh
@@ -1,12 +1,11 @@
 set -e
 
 alias python="/usr/bin/env python"
-alias pip="python -m pip"
 
-pip install build twine
+uv pip install build twine
 
 rm dist/ -rf
 python -m build
 python -m twine check dist/*
 
-echo "done, now run `python -m twine check dist/*`"
+echo "done, now run `python -m twine upload dist/*`"
